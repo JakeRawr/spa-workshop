bcf0de2c9807de4363160adb00ff03a170242ae1
wentingrohwer <wenting.rohwer@gmail.com>
2014-12-03 13:58:53 -0800

    added wendy stuff

--- app/js/controllers/cityCtrl.js
+++ app/js/controllers/cityCtrl.js
@@ -1,12 +1,10 @@
 
 angular.module('controllers').controller('CityCtrl', function($scope, $routeParams, $log, geoLocation, forecast, news) {
   'use strict';
-
   this.cityName = $routeParams.city;
-
+  //$routeParams is the
   // Need to assign this to another variable in order to use it in nested contexts.
   var self = this;
-
   // Flatten the promise chain for better readability.
   // http://solutionoptimist.com/2013/12/27/javascript-promise-chains-2/
   geoLocation(this.cityName)
