a6bffea2934821c89c723cd004f23506e3035306
Jake Yang <tsunghan614@gmail.com>
2014-12-03 12:40:10 -0800

    workshop refactor

---
+++ app/js/controllers/cityCtrl.js
@@ -0,0 +1,33 @@
+angular.module('controllers').controller('CityCtrl', function($scope, $routeParams, $log, geoLocation, forecast, news) {
+  'use strict';
+
+  this.cityName = $routeParams.city;
+
+  // Need to assign this to another variable in order to use it in nested contexts.
+  var self = this;
+
+  // Flatten the promise chain for better readability.
+  // http://solutionoptimist.com/2013/12/27/javascript-promise-chains-2/
+  geoLocation(this.cityName)
+    .then(function(latLong) {
+      return forecast(latLong[0], latLong[1]);
+    })
+    .then(function(forecast) {
+      self.forecast = forecast.data;
+      self.currentFore = forecast.data.currently;
+      return news(self.cityName);
+    })
+    .then(function(news) {
+      self.news = news;
+      self.listNews = [];
+      for(var i = 0; i < news.length; i++){
+        var newsObj = {};
+        newsObj.title = news[i].title;
+        newsObj.url = news[i].href.split('url=')[1];
+        self.listNews.push(newsObj);
+      }
+    })
+    .catch(function(err) {
+      $log.error(err);
+    });
+});
